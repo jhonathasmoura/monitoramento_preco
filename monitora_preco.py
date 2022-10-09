d1386adedb780f2ce7ac9d2b8d90a801b9867fd2
jhonathasmoura <jhonathasmoura6@gmail.com>
2022-10-09 17:30:09 -0300

    Adicionado envio de email para múltiplos destinatários

--- monitora_preco.py
+++ monitora_preco.py
@@ -1,9 +1,11 @@
 import urllib.request
-import smtplib
-import getpass
+import yagmail
 import os
-from email.mime.text import MIMEText
+import getpass
 from time import sleep
+
+
+os.system("cls" if os.name == "nt" else "clear")
 
 
 def consulta_preco(url):
@@ -15,38 +17,35 @@ def consulta_preco(url):
     return float(preco)
 
 
-def envia_email(email_remetente, senha, email_destinatario, assunto, mensagem):
-    msg = MIMEText(mensagem)
-    msg["FROM"] = email_remetente
-    msg["TO"] = ", ".join(email_destinatario)
-    msg["SUBJECT"] = assunto
+def envia_email(email_remetente, senha, email_destinatario, assunto, preco, url, mensagem):
+    server = yagmail.SMTP(email_remetente, senha)
+    server.send(email_destinatario, assunto, mensagem)
 
-    server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-    server.login(email_remetente, senha)
-    server.sendmail(email_remetente, email_destinatario, msg.as_string())
-    server.quit()
-
-    print(f"Email enviado para <{email_destinatario[0]}> com link de compra!")
+    print(f"Email com link de compra enviado para: ")
+    for email in email_destinatario:
+        print(f"<{email}>")
 
 
 if __name__ == "__main__":
-    if os.path.isfile("login.txt"):
-        arquivo = open("login.txt", "r", encoding="utf-8")
+    if os.path.isfile("dados.txt"):
+        arquivo = open("dados.txt", "r", encoding="utf-8")
     else:
-        arquivo = open("login.txt", "w", encoding="utf-8")
-        arquivo.write(input("Digite o seu email: ") + "\n")
+        arquivo = open("dados.txt", "w", encoding="utf-8")
+        arquivo.write(input("Digite o seu email: ") + " ")
         senha = getpass.getpass("Digite a senha: ")
         arquivo.write(f"{senha}\n")
-        arquivo.write(input("Digite o email do destinatário: "))
-        print("Arquivo de login criado!")
+        qtd_destinatarios = int(input("Deseja enviar a oferta para quantos destinatários? "))
+        for i in range(qtd_destinatarios):
+            arquivo.write(input(f"Digite o email do destinatário{'' if qtd_destinatarios == 1 else f' {i + 1}'}: ") + " ")
+        print("Arquivo com informações de login e destinatários criado!")
         arquivo.close()
 
-        arquivo = open("login.txt", "r", encoding="utf-8")
+        arquivo = open("dados.txt", "r", encoding="utf-8")
 
-    login = [dado.strip() for dado in arquivo.readlines()]
-    email_remetente = login[0]
-    senha = login[1]
-    email_destinatario = [login[2]]
+    dados = [dado.strip().split(' ') for dado in arquivo.readlines()]
+    email_remetente = dados[0][0]
+    senha = dados[0][1]
+    email_destinatario = dados[1]
     assunto = "Alerta de Preço!"
     arquivo.close()
 
@@ -64,17 +63,19 @@ if __name__ == "__main__":
         if preco_normal < 4.70 or preco_promocional < 4.70:
             if min(preco_normal, preco_promocional) == preco_normal:
                 print(f"Preço baixo encontrado.")
-                mensagem = f"O preço do café baixou para US${preco_normal:.2f}\nCompre agora em: {urls[0]}"
-                envia_email(
-                    email_remetente, senha, email_destinatario, assunto, mensagem
-                )
+                mensagem = [
+                        f"O preço do café baixou para US${preco_normal:.2f}", 
+                        f"Compre agora em: {urls[0]}"
+                ]
+                envia_email(email_remetente, senha, email_destinatario, assunto, preco_normal, urls[0], mensagem)
                 break
             elif min(preco_normal, preco_promocional) == preco_promocional:
                 print(f"Preço baixo encontrado.")
-                mensagem = f"O preço do café baixou para US${preco_promocional:.2f}\nCompre agora em: {urls[1]}"
-                envia_email(
-                    email_remetente, senha, email_destinatario, assunto, mensagem
-                )
+                mensagem = [
+                        f"O preço do café baixou para US${preco_promocional:.2f}",
+                        f"Compre agora em: {urls[1]}"
+                ]
+                envia_email(email_remetente, senha, email_destinatario, assunto, preco_promocional, urls[1], mensagem)
                 break
         else:
             print("Preço alto! Aguarde um pouco para comprar.")
